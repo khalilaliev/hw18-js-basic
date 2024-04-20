9fbe576d073afd0f074a7e57e04e721197c48784
andrii.vorflik <andrii.vorflik@gmail.com>
2024-04-20 22:19:43 +0300

    added loading and error

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+fetch("http://localhost:3000/posts")
+  .then((response) => {
+    console.log("response:", response);
+    return response.json();
+  })
+  .then((response) => {
+    console.log("response:", response);
+  });
